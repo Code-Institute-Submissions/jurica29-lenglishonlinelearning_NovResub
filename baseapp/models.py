c08f20aacce046b4122e789319612fa9e15e4829
jurica29 <jrcpuljiz@gmail.com>
2022-08-17 19:52:05 +0000

    Create item model in models.py of the base app

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = {
+    ('G', 'Group'),
+    ('I', 'Individual')
+}
+
+LABEL_CHOICES = {
+    ('P', 'Package of lessons'),
+    ('S', 'Single lesson')
+}
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=5)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=5)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
