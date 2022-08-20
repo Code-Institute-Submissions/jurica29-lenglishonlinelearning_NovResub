04e1764d918a505af3619813af67de3d9738f9af
jurica29 <jrcpuljiz@gmail.com>
2022-08-20 08:07:19 +0000

    Add class based views for home and product detail page

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import View, DetailView, ListView
 # Create your views here.
 
-def home(request):
-    context = {
-        'items': Item.objects.all()
-    }
+class HomeView(ListView):
+    model = Item
+    paginate_by = 6
+    template_name = 'home.html'
 
-    return render(request, 'home.html', context=context)
+class ProductDetailView(DetailView):
+    model = Item
+    template_name = 'productdetail.html'
