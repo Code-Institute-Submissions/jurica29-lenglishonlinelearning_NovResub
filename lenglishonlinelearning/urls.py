90e6867a21c66516b0ff96b36671248904b93db8
jurica29 <jrcpuljiz@gmail.com>
2022-08-31 16:49:47 +0000

    Customise login, logout and signup templates of allauth

--- lenglishonlinelearning/urls.py
+++ lenglishonlinelearning/urls.py
@@ -22,8 +22,8 @@ from lenglishonlinelearning import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('baseapp.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('baseapp.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
