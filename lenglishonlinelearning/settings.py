7a8a3a3ed32a31ebe380347a9de5996fc81db9fa
jurica29 <jrcpuljiz@gmail.com>
2022-09-23 18:30:26 +0000

    Move js for stripe to separate file

--- lenglishonlinelearning/settings.py
+++ lenglishonlinelearning/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-
-# g8p_6liyi3mww2pfjgzk3977hxtb-ht40*1*pn+_fb2^@7=wpm
+SECRET_KEY = os.environ.get('SECRET_KEY', 'g8p_6liyi3mww2pfjgzk3977hxtb-ht40*1*pn+_fb2^@7=wpm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
