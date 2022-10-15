600872e778d31934a6d20ce7294afad984264ace
jurica29 <jrcpuljiz@gmail.com>
2022-10-15 11:02:05 +0000

    Beuatify html, css and js code

--- lenglishonlinelearning/settings.py
+++ lenglishonlinelearning/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-jurica29-lenglishonline-iqa7g6exbp0.ws-eu71.gitpod.io"
