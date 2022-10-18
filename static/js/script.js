54560f15ddf0496f9114e6c799d0394f213b6e64
jurica29 <jrcpuljiz@gmail.com>
2022-10-18 20:14:23 +0000

    Revise html code and fix errors

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 // Code responsible for toasts showing up
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastElList = [].slice.call(document.querySelectorAll('.toast'));
 var toastList = toastElList.map(function(toastEl) {
-	return new bootstrap.Toast(toastEl)
+	return new bootstrap.Toast(toastEl);
 });
 toastList.forEach(toast => {
-	toast.show()
+	toast.show();
 });
